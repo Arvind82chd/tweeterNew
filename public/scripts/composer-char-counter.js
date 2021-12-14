ea88d7b40f260c5db1e4db5b789b9defb3d1f231
Arvind Sharma <mail.arvind.s@gmail.com>
2021-12-14 23:42:00 +0000

    Added the old tweets section

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,16 @@
 
 $(document).ready(function () {
 
-  $('#tweet-text').on("input", (event) => {
+  $('#tweet-text').on("input", (event) => {// picks the input value from id tweet-text from textarea in html implements .on for eventlistener 'input' not keypress as key press in dep and others have some limitation.
     console.log(event.target.value);
-    let count = event.target.value.length;
-    const counter = $('#count');
-    if (count >= 140) {
+    let count = event.target.value.length;// responsible for giving the length of key presses.
+    const counter = $('#count');//picks up the count display on html and connects to this variable
+    if (count >= 140) { // checks if more than given limit of characters clicked or not.
         $('#count').css("color","red")
     } else {
         $('#count').css("color","black")
     }
-    counter.text(`${140 - count}`);
+    counter.text(`${140 - count}`);//displays the counter at the count location in html 
   });
   
   
