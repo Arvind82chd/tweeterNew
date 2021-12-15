4e2480f3e16c8697c7ad14ec2988ead0ddab8d80
Arvind Sharma <mail.arvind.s@gmail.com>
2021-12-15 20:09:02 +0000

    added and tested createTweetElement function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,47 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//Tweet database:
+
+const tweetDataBase =
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1639342241661
+  };
+
+const createTweetElement = function() {
+
+  const tweet = `<article>
+ <section class="old-tweets">
+        <header class="topLine">
+          <div class="topLineLeft">
+            <img alt="avtar" src=${tweetDataBase.user.avatars}>
+            <h3>${tweetDataBase.user.name}</h3>
+          </div>
+          
+          <h3>${tweetDataBase.user.handle}</h3>
+        </header>
+        <p class="tweetsOutput" name="tweets">${tweetDataBase.content.text}</p>
+        <footer class="belowTweet">
+          <output class="timestamp">${tweetDataBase.created_at}</output>
+          <div class="icons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+        </footer>
+      </section>
+
+  </article>`;
+
+  return tweet;
+}
+
+console.log(createTweetElement(tweetDataBase));
