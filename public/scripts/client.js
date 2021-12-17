82bd9954d7276247cb1580aae0180b0000514d5f
Arvind Sharma <mail.arvind.s@gmail.com>
2021-12-17 04:25:58 +0000

    Final updates

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const escape = function (str) { //to check the cross scripting or improve safety in taking inputs from user.
+const escape = function(str) { //to check the cross scripting or improve safety in taking inputs from user.
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
@@ -34,45 +34,44 @@ const createTweetElement = function(data) {
 };
   
   
-  const renderTweets = function(data) {
-    const tweetContainer = $("#allTweets")//to send the element to html
-    //const reverseData = data.reverse();//to reverse the direction of tweet sequence
-    for (let tweet of data) {
-      const element = createTweetElement(tweet);
-      console.log(element);
-      tweetContainer.prepend(element)//to send the tweet text to the required place.
-    }
+const renderTweets = function(data) {
+  const tweetContainer = $("#allTweets");
+  for (let tweet of data) {
+    const element = createTweetElement(tweet);
+    console.log(element);
+    tweetContainer.prepend(element);//to send the tweet text to the required place.
   }
+};
 
-  const loadTweets = function() { //picked it from my previous code
-   $.get("/tweets", function (data) { //uses ajax-jquery get method to connect with the data on tweets page.
-     renderTweets(data);
-   })
-  };
+const loadTweets = function() {
+  $.get("/tweets", function(data) {
+    renderTweets(data);
+  });
+};
 
 
-  $(document).ready(function () {
+$(document).ready(function() {
     
-    loadTweets();//So that all tweets apear before new tweet entry
-    $(".limitExceedError").hide() //to hide the error until condition met 
-    $(".emptyTextError").hide()
+  loadTweets();//So that all tweets apear before new tweet entry
+  $(".limitExceedError").hide(); //to hide the error until condition met
+  $(".emptyTextError").hide();
 
-    $(".tweetForm").submit(function(event) {
-    event.preventDefault();//to cancel the submit action initially until it is submitted manually.
+  $(".tweetForm").submit(function(event) {
+    event.preventDefault();
     
     const tweetText = $("#tweet-text");
     const counter = tweetText.val().length;
     
-    if (counter === 0 || tweetText.val() === null) {//condition to check data entered or not from previous code
+    if (counter === 0 || tweetText.val() === null) {
       $(".emptyTextError").text("Enter some valid data!!!").show();
-      return
+      return;
     }
-     $.post("/tweets", { text: tweetText.val() }, function () { //assigns the value captured from texarea to text in database.
+    $.post("/tweets", { text: tweetText.val() }, function() {
     
       tweetText.val(''); //to clear the textarea
       $("#allTweets").empty(); // empty's data and refreshes it with new one in next step
-      $('#count').val("140"); // resets the counter to 140 
+      $('#count').val("140"); // resets the counter to 140
       loadTweets();//to load the tweets.
-  })
- })
+    });
+  });
 });
